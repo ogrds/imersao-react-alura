d9847a8b1cb429ce52951e665ffd8d65d359eb74
Gabriel Ribeiro <gabrielrsilva98@gmail.com>
2021-02-16 18:14:34 -0300

    teste API route

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,13 +1,4 @@
-import db from "../../db.json";
-
 export default function (req, res) {
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-
-  res.json(db);
+  res.statusCode = 200;
+  res.json({ name: 'John Doe' });
 }
